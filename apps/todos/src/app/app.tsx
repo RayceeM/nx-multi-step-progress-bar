46453611b703bc0588e16352f4ab2322a93a5cd7
Racheal <racheal@Racheals-MacBook-Pro.local>
2022-09-21 19:17:14 +0300

    Create a multi-step progress bar

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,14 +1,59 @@
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import StepProgressBar from 'react-step-progress';
+import 'react-step-progress/dist/index.css';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  const step1Content = <h1>Step 1 Content</h1>;
+  const step2Content = <h1>Step 2 Content</h1>;
+  const step3Content = <h1>Step 3 Content</h1>;
+
+  const step2Validator = () => {
+    return true;
+  };
+
+  const step3Validator = () => {
+    // return a boolean
+  };
+
+  function onFormSubmit() {
+    // handle the submit logic here
+    // This function will be executed at the last step
+    // when the submit button (next button in the previous steps) is pressed
+  }
+
   return (
     <StyledApp>
-      <NxWelcome title="todos" />
+      <h1>Multi step Progress Bar</h1>
+      <StepProgressBar
+        startingStep={0}
+        onSubmit={onFormSubmit}
+        steps={[
+          {
+            label: 'Step 1',
+            subtitle: '10%',
+            name: 'step 1',
+            content: step1Content,
+          },
+          {
+            label: 'Step 2',
+            subtitle: '50%',
+            name: 'step 2',
+            content: step2Content,
+            validator: step2Validator,
+          },
+          {
+            label: 'Step 3',
+            subtitle: '100%',
+            name: 'step 3',
+            content: step3Content,
+          },
+        ]}
+      />
+      ;
     </StyledApp>
   );
 }
